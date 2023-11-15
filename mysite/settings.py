9d6fc39948dbf044f62c914c6652fee4c6b6b784
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2023-11-15 23:15:26 +0330

    sssss
    ssss

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,8 +50,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+ 
 ROOT_URLCONF = 'mysite.urls'
+
 
 TEMPLATES = [
     {
