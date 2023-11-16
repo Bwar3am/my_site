afdbc685c23e4a0cc2fbe26db3e8d35330ced5f1
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2023-11-16 23:05:04 +0330

    we created a template
    creted a template completely

--- website/views.py
+++ website/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 
 def index_view(request):
-    return render(request,'website/index.html')
+    return render(request,'index.html')
 
 def about_view(request):
-    return render(request,'website/about.html')
+    return render(request,'about.html')
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    return render(request,'contact.html')
