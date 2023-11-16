d5ae44dbe163f0eda53aed68412e077a25eca633
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2023-11-16 23:47:14 +0330

    we created a general template
    we craeted a general template

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
