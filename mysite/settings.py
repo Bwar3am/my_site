a279dedafe1e761edefc2770adc3f3ed0f0c57d0
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2023-11-21 00:29:00 +0330

    statics files
    we created an statics file on project

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_URL = BASE_DIR = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR  / "media"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+    
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
